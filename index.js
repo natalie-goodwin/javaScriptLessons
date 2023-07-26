e62f3d4f9c734f683e9370928ef320a5a42f9180
Natalie Goodwin <nmgoodwin27@gmail.com>
2023-07-26 17:33:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log('100')
+console.log(20, 'Hello', true)
+console.table({name: 'Natalie', email: 'ngood@gmail.com'})
+
+console.group('simple')
+console.log('x')
+console.error('Alert')
+console.warn('Warning')
+console.groupEnd()
+
